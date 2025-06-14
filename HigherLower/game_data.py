1c333ce1a7e45b6313db3fb3e55668075415360d
Tarunikaa B <tarunikatiku23@gmail.com>
2025-06-14 13:04:34 +0530

    Add files via upload

---
+++ HigherLower/game_data.py
@@ -0,0 +1,26 @@
+data = [
+    {
+        'name': 'Tarunikaa',
+        'follower_count': 600,
+        'description': 'Dancer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Krish',
+        'follower_count': 460,
+        'description': 'MD',
+        'country': 'UK'
+    },
+    {
+        'name': 'Baga',
+        'follower_count': 340,
+        'description': 'HR',
+        'country': 'India'
+    },
+    {
+        'name': 'Abinav',
+        'follower_count': 500,
+        'description': 'student',
+        'country': 'Bali'
+    }
+]
